ccc7bbbe8c2552690b76dc0bfc4cfc2fa16f912d
June Rhodes <jrhodes@redpointsoftware.com.au>
2015-11-19 22:14:42 +1100

    Add required call for iOS executables

--- Prototest.Include/Program.cs
+++ Prototest.Include/Program.cs
@@ -1,5 +1,9 @@
+#if PLATFORM_IOS
+using UIKit;
+#else
 using System;
 using System.Reflection;
+#endif
 
 namespace Prototest.Include
 {
@@ -7,12 +11,16 @@ namespace Prototest.Include
     {
         public static void Main(string[] args)
         {
+#if PLATFORM_IOS
+            UIApplication.Main(args, null, "AppDelegate");
+#else
             if (Prototest.Library.Runner.Run(Assembly.GetExecutingAssembly()))
             {
                 Environment.Exit(0);
             }
 
             Environment.Exit(1);
+#endif
         }
     }
 }
